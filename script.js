5b45f2e03ddfcb97bb769ccc6c38a6b9b566b46c
Pobbati Surendra <surendrapobbati@gmail.com>
2023-07-11 18:54:48 +0530

    Fixed an Error where element is draggable.

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const container = document.querySelector(".container")
 
-for(let i=0; i<16; i++) {
+let mouseDown = false;
+document.body.onmousedown = () => {mouseDown = true};
+document.body.onmouseup = () => {mouseDown = false};
+
+for(let i=0; i<30; i++) {
     const gridRow = document.createElement("div");
     gridRow.classList.add("row")
+    gridRow.setAttribute("draggable", "false")
     container.appendChild(gridRow);
-    for(let j=0; j<16; j++){
+    for(let j=0; j<30; j++){
         const gridColumn = document.createElement("div");
+        gridColumn.setAttribute("draggable", "false")
         gridColumn.classList.add("column", "border-top-left")
         gridRow.appendChild(gridColumn)        
     }
     console.log("done");
+}
+
+const columns = document.querySelectorAll(".column");
+let penColor = "#000000";
+columns.forEach(column => {
+    column.addEventListener('mouseover', paintGrid);
+    column.addEventListener('mousedown', paintGrid);
+});
+
+function paintGrid(e) {
+    if(e.type === 'mouseover' && !mouseDown) return;  
+    e.target.style.backgroundColor = penColor;
 }
